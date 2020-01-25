7b56a22ba9a435b5a9d618becaf8e2098e28b63f
Ritsu <ritsu@MacBook-Pro-de-Ritsu.local>
2020-01-25 11:03:52 +0100

    Video 2

--- Rain/src/com/paumarin/rain/Game.java
+++ Rain/src/com/paumarin/rain/Game.java
@@ -1,9 +1,24 @@
 package com.paumarin.rain;
 
-public class Game {
+public class Game implements Runnable{
 	
 	public static int WIDTH = 300;
 	public static int HEIGHT = WIDTH / 16 * 9;
 	public static int SCALE = 3;
+	
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
