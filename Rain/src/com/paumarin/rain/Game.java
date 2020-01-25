d2b2a56b29275c99ba8239b25e3584c7d1c896ae
Ritsu <ritsu@MacBook-Pro-de-Ritsu.local>
2020-01-25 11:50:10 +0100

    Video 5

--- Rain/src/com/paumarin/rain/Game.java
+++ Rain/src/com/paumarin/rain/Game.java
@@ -2,6 +2,7 @@ package com.paumarin.rain;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -45,8 +46,22 @@ public class Game extends Canvas implements Runnable{
 
 	public void run() {
 		while(running) {
-			System.out.println("Running...");
+			update();
+			render();
 		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
 	}
 	
 	public static void main(String[] args) {
