71844cffba70289887b1a5fd1ae97714496eebf1
Pau-Marin <ritsu95@icloud.com>
2023-01-23 23:25:10 +0100

    Video 89

--- Rain/src/com/paumarin/rain/entity/mob/Player.java
+++ Rain/src/com/paumarin/rain/entity/mob/Player.java
@@ -14,7 +14,13 @@ public class Player extends Mob {
 
 	private Keyboard input;
 	private Sprite sprite;
-	private AnimatedSprite test = new AnimatedSprite(SpriteSheet.player_down, 32, 32, 3);
+
+	private AnimatedSprite down = new AnimatedSprite(SpriteSheet.player_down, 32, 32, 3);
+	private AnimatedSprite up = new AnimatedSprite(SpriteSheet.player_up, 32, 32, 3);
+	private AnimatedSprite left = new AnimatedSprite(SpriteSheet.player_left, 32, 32, 3);
+	private AnimatedSprite right = new AnimatedSprite(SpriteSheet.player_right, 32, 32, 3);
+
+	private AnimatedSprite animSprite = down;
 
 	private int anim = 0;
 	private boolean walking = false;
@@ -35,15 +41,30 @@ public class Player extends Mob {
 	}
 
 	public void update() {
-		test.update();
+		if (walking) animSprite.update();
+		else animSprite.setFrame(0);
+
 		if (fireRate > 0) fireRate--;
 		int xa = 0, ya = 0;
 		if (anim < 7500) anim++;
 		else anim = 0;
-		if (input.up) ya--;
-		if (input.down) ya++;
-		if (input.left) xa--;
-		if (input.right) xa++;
+
+		if (input.up) {
+			ya--;
+			animSprite = up;
+		}
+		if (input.down) {
+			ya++;
+			animSprite = down;
+		}
+		if (input.left) {
+			xa--;
+			animSprite = left;
+		}
+		if (input.right) {
+			xa++;
+			animSprite = right;
+		}
 
 		if (xa != 0 || ya != 0) {
 			move(xa, ya);
@@ -77,48 +98,19 @@ public class Player extends Mob {
 
 	public void render(Screen screen) {
 		int flip = 0;
-		if (dir == 0) {
-			sprite = Sprite.player_forward;
-			if (walking) {
-				if (anim % 20 > 10) {
-					sprite = Sprite.player_forward_1;
-				} else {
-					sprite = Sprite.player_forward_2;
-				}
-			}
-		}
-		if (dir == 1) {
-			sprite = Sprite.player_side;
-			if (walking) {
-				if (anim % 20 > 10) {
-					sprite = Sprite.player_side_1;
-				} else {
-					sprite = Sprite.player_side_2;
-				}
-			}
-		}
-		if (dir == 2) {
-			sprite = Sprite.player_back;
-			if (walking) {
-				if (anim % 20 > 10) {
-					sprite = Sprite.player_back_1;
-				} else {
-					sprite = Sprite.player_back_2;
-				}
-			}
-		}
-		if (dir == 3) {
-			flip = 1;
-			sprite = Sprite.player_side;
-			if (walking) {
-				if (anim % 20 > 10) {
-					sprite = Sprite.player_side_1;
-				} else {
-					sprite = Sprite.player_side_2;
-				}
-			}
-		}
-		sprite = test.getSprite();
+		/*
+		 * if (dir == 0) { sprite = Sprite.player_forward; if (walking) { if (anim % 20
+		 * > 10) { sprite = Sprite.player_forward_1; } else { sprite =
+		 * Sprite.player_forward_2; } } } if (dir == 1) { sprite = Sprite.player_side;
+		 * if (walking) { if (anim % 20 > 10) { sprite = Sprite.player_side_1; } else {
+		 * sprite = Sprite.player_side_2; } } } if (dir == 2) { sprite =
+		 * Sprite.player_back; if (walking) { if (anim % 20 > 10) { sprite =
+		 * Sprite.player_back_1; } else { sprite = Sprite.player_back_2; } } } if (dir
+		 * == 3) { flip = 1; sprite = Sprite.player_side; if (walking) { if (anim % 20 >
+		 * 10) { sprite = Sprite.player_side_1; } else { sprite = Sprite.player_side_2;
+		 * } } }
+		 */
+		sprite = animSprite.getSprite();
 		screen.renderPlayer(x - 16, y - 16, sprite, flip);
 	}
 
