f9e961378be202a1e2123eec91128b0e0c271a3b
Pau-Marin <ritsu95@icloud.com>
2023-03-24 17:09:23 +0100

    Video 107

--- Rain/src/com/paumarin/rain/entity/mob/Shooter.java
+++ Rain/src/com/paumarin/rain/entity/mob/Shooter.java
@@ -112,7 +112,6 @@ public class Shooter extends Mob {
 	}
 
 	public void render(Screen screen) {
-		Debug.drawRect(screen, 17 * 16, 57 * 16, 60, 40, true);
 		sprite = animSprite.getSprite();
 		screen.renderMob(x - 16, y - 16, this);
 	}
---
+++ Rain/src/com/paumarin/rain/graphics/Font.java
@@ -0,0 +1,11 @@
+package com.paumarin.rain.graphics;
+
+public class Font {
+
+	private static SpriteSheet font = new SpriteSheet("/fonts/arial.png", 16);
+
+	public Font() {
+		
+	}
+
+}
